b6e2510975f2a1dcda71974a09736228cb16a036
Shubham Pandey <shubhampandey979271@gmail.com>
2021-11-29 07:43:08 +0530

    Subscribe Papaya Coders

---
+++ app/src/main/java/in/papaya/googleias/MainActivity.kt
@@ -0,0 +1,96 @@
+package `in`.papaya.googleias
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import com.anjlab.android.iab.v3.BillingProcessor
+import com.anjlab.android.iab.v3.PurchaseInfo
+
+class MainActivity : AppCompatActivity(), BillingProcessor.IBillingHandler {
+
+    private lateinit var buyNow: Button
+
+    private lateinit var bp: BillingProcessor
+    private lateinit var purchaseInfo: PurchaseInfo
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        buyNow = findViewById(R.id.buyNow)
+
+        bp = BillingProcessor(
+            this,
+            "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAknyN4onvALigi487je57FHHbgQZDm+IWd84nBf3ED50yUuwe4T+ZJ/eMPCe1rpsbxjY0IO+tiudorIjAoneVo4vizyfYXxuSjNFhD6d9Ozwy4+j2Zmoh7XSjnmXR2bDZxOd/oz+vr9jaO2UCrXoqx7KxFb9aB2zHARcHiOlk1BRhrSBfDhS8nJVwWI1BP5Z3ccSPLEWMbP5pt7PyuScPrs5JZS92LeyARcRnqPmjvcK0itO6fYISdoMFRv9466c1I4oa8HHtDShHPSN2o8ZPV8oESe9l+QEzUrrLnK6YvnPOdl2/jznHiuzJP9HeNRvsR83FsFem0tVJaf764Miq1wIDAQAB",
+            this
+        )
+        bp.initialize()
+
+        buyNow.setOnClickListener {
+            bp.subscribe(this, "papayacoders")
+        }
+    }
+
+    override fun onProductPurchased(productId: String, details: PurchaseInfo?) {
+        Toast.makeText(this, "Successfull", Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onPurchaseHistoryRestored() {
+//        TODO("Not yet implemented")
+    }
+
+    override fun onBillingError(errorCode: Int, error: Throwable?) {
+//        TODO("Not yet implemented")
+    }
+
+    override fun onBillingInitialized() {
+//        TODO("Not yet implemented")
+
+        bp.loadOwnedPurchasesFromGoogleAsync(object : BillingProcessor.IPurchasesResponseListener {
+            override fun onPurchasesSuccess() {
+//                TODO("Not yet implemented")
+            }
+
+            override fun onPurchasesError() {
+//                TODO("Not yet implemented")
+            }
+
+        })
+
+        if (bp.getSubscriptionPurchaseInfo("papayacoders") != null) {
+
+            purchaseInfo = bp.getSubscriptionPurchaseInfo("papayacoders")!!
+
+
+            if (purchaseInfo != null) {
+                if (purchaseInfo.purchaseData.autoRenewing) {
+                    Toast.makeText(this, "Already subscribe", Toast.LENGTH_SHORT).show()
+                } else Toast.makeText(this, "Not subscribed", Toast.LENGTH_SHORT).show()
+            } else Toast.makeText(this, "Expired", Toast.LENGTH_SHORT).show()
+        }
+
+    }
+
+    override fun onDestroy() {
+        if (bp != null){
+            bp.release()
+        }
+        super.onDestroy()
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
